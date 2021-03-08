b162368cc1ae6b2705f580db465261f2b5fcdfea
Simasq <simas.kud@gmail.com>
2021-03-08 02:20:10 +0200

    Make footer stick to bottom when no content

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,19 +2,21 @@ import { Link } from 'react-router-dom'
 import styles from './Footer.module.scss'
 
 const Footer = () => (
-    <footer className={`${styles['Footer']} mx-auto`}>
-        <div className={`${styles[`Footer__innerWrapper`]} px-4 my-8 lg-my-9`}>
-            <div className="size-6 textWeight-semibold">
-                <div className={`${styles['Footer__title']} color-grey mb-4`}>Engage</div>
-                <Link to="/" className={`${styles['Footer__link']} mb-3`}>
-                    Privacy Pilicy
-                </Link>
-                <Link to="/" className={styles['Footer__link']}>
-                    Terms of Service
-                </Link>
-            </div>
-            <div className={`${styles['Footer__copyright']} textWeight-semibold size-7`}>
-                Copyright © 2020 NordPass.com
+    <footer className={`${styles['Footer__wrapper']}`}>
+        <div className={`${styles['Footer']} mx-auto`}>
+            <div className={`${styles[`Footer__innerWrapper`]} px-4 my-8 lg-my-9`}>
+                <div className="size-6 textWeight-semibold">
+                    <div className={`${styles['Footer__title']} color-grey mb-4`}>Engage</div>
+                    <Link to="/" className={`${styles['Footer__link']} mb-3`}>
+                        Privacy Pilicy
+                    </Link>
+                    <Link to="/" className={styles['Footer__link']}>
+                        Terms of Service
+                    </Link>
+                </div>
+                <div className={`${styles['Footer__copyright']} textWeight-semibold size-7`}>
+                    Copyright © 2020 NordPass.com
+                </div>
             </div>
         </div>
     </footer>
